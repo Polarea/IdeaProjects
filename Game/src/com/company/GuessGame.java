7c72fdf168b7639baa212dd067b47747b7710244
Muhammad Rizwan <rizvawn@gmail.com>
2023-11-12 17:16:18 +0100

    Adding all the content in the folder IdeaProjects

---
+++ Game/src/com/company/GuessGame.java
@@ -0,0 +1,16 @@
+package com.company;
+public class GuessGame {
+Player p1;
+Player p2;
+Player p3;
+pulbic void StartGame() {
+    p1 = new Player ();
+    p2 = new Player ();
+    p3 = new Player ();
+    int guessp1 = 0;
+    int guessp2 = 0;
+    int guessp3 = 0;
+    boolean p1isRight =
+    }
+}
+
